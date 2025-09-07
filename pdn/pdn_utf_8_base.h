ddf4fbb804afaaaa7837e72d27acee1908354500
SunflavourPetals <3090002881@qq.com>
2025-09-07 18:52:29 +0800

    typo

--- pdn/pdn_utf_8_base.h
+++ pdn/pdn_utf_8_base.h
@@ -8,13 +8,16 @@
 
 namespace pdn::unicode::utf_8
 {
-	using code_unit_t                 = utf_8_code_unit_t;
-	template <typename traits         = ::std::char_traits<code_unit_t>, typename alloc = ::std::allocator<code_unit_t>>
-	using basic_code_unit_string      = ::std::basic_string<code_unit_t, traits, alloc>;
-	using code_unit_string            = basic_code_unit_string<>;
-	template <typename traits         = ::std::char_traits<code_unit_t>>
+	using code_unit_t = utf_8_code_unit_t;
+
+	template <typename traits = ::std::char_traits<code_unit_t>, typename alloc = ::std::allocator<code_unit_t>>
+	using basic_code_unit_string = ::std::basic_string<code_unit_t, traits, alloc>;
+
+	template <typename traits = ::std::char_traits<code_unit_t>>
 	using basic_code_unit_string_view = ::std::basic_string_view<code_unit_t, traits>;
-	using code_unit_string_view       = basic_code_unit_string_view<>;
+
+	using code_unit_string = basic_code_unit_string<>;
+	using code_unit_string_view = basic_code_unit_string_view<>;
 
 	constexpr bool is_trailing(code_unit_t c) noexcept
 	{
