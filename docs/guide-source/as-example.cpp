455a7e4aaf77bda33e71a13d846c1dba8449cab0
SunflavourPetals <3090002881@qq.com>
2026-06-20 19:19:16 +0800

    add as-example.cpp

---
+++ docs/guide-source/as-example.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+#include "pdn_parse.h"
+#include "pdn_data_entity.h"
+
+#include "outu8sv.h"
+
+int main()
+{
+	using namespace pdn;
+	using namespace std::string_view_literals;
+
+	auto e = parse(u8R"(
+			f   : 1.25
+			inf : @inf
+			c   : '爱'
+			nan : @nan
+			s   : "string"
+		)"sv, utf8_tag);
+
+	std::cout << as_int(e[u8"f"]) << "\n"; // 1
+
+	std::cout << as_int(e[u8"inf"]) << "\n"; // 9223372036854775807
+
+	std::cout << std::boolalpha << as_bool(e[u8"c"]) << "\n"; // true
+
+	std::cout << as_int(e[u8"nan"]) << "\n"; // 0
+
+	std::cout << as_string(e[u8"f"]) << "\n"; // *null string
+}
