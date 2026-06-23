e9286e28f49049c563ad6b4191ce1c0c6752eea5
SunflavourPetals <3090002881@qq.com>
2026-06-23 18:08:29 +0800

    add alias for pdn::type::character/string/list/object<char8/16/32_t>

--- pdn/pdn_type.h
+++ pdn/pdn_type.h
@@ -3,6 +3,7 @@
 
 #include "pdn_type_basic.h"
 #include "pdn_type_config.h"
+#include "pdn_unicode_base.h"
 #include "pdn_entity_forward_decl.h"
 
 namespace pdn::type
@@ -14,6 +15,22 @@ namespace pdn::type
 
 	template <typename char_t>
 	using object = config::object<string<char_t>, entity<char_t>>;
+
+	using u8char  = character<unicode::u8char_t>;
+	using u16char = character<unicode::u16char_t>;
+	using u32char = character<unicode::u32char_t>;
+
+	using u8string  = string<unicode::u8char_t>;
+	using u16string = string<unicode::u16char_t>;
+	using u32string = string<unicode::u32char_t>;
+
+	using u8list  = list<unicode::u8char_t>;
+	using u16list = list<unicode::u16char_t>;
+	using u32list = list<unicode::u32char_t>;
+
+	using u8object  = object<unicode::u8char_t>;
+	using u16object = object<unicode::u16char_t>;
+	using u32object = object<unicode::u32char_t>;
 }
 
 #endif
